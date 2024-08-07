ff75c9456a0af6b293a85c48c7505513c92c19d8
Matthew <matthewrmuller@gmail.com>
2024-08-07 10:29:08 -0600

    commit

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-// search by name http://www.omdbapi.com/?apikey=19c5e51c&s=
-// search by id https://www.omdbapi.com/?apikey=19c5e51c&i=
-// async function renderPoster(id) {
-//
-// }
-const featuredPosters = document.querySelector('.row')
+const API_SEARCH = 
+"http://www.omdbapi.com/?apikey=19c5e51c&s=";
+
+const API_ID = 
+"https://www.omdbapi.com/?apikey=19c5e51c&i=";
+
+
 const form = document.querySelector("form");
+let movieArr = [""]
 
 form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+  event.preventDefault();``
   let input = document.getElementById("userInput").value;
-  const response = await fetch(
+  let movie = await fetch(
     `http://www.omdbapi.com/?apikey=19c5e51c&s=${input}`
   );
-  console.log(await response.json());
+  const result = await movie.json();
 });
 
 
-async function posters(id) {
-  const poster = await fetch(`http://www.omdbapi.com/?apikey=19c5e51c&s=fire`)
-  const posterData = await poster.json()
-  featuredPosters.innerHTML = posterData.map((element__wrapper) => posterHTML(element__wrapper)).join('')
-}
 
-function posterHTML(element__wrapper) {
-  return `<div class="element__wrapper">
-  <div class="img__wrapper">
-    <img src="${element__wrapper.Poster}" alt="" />
-  </div>
-  <div class="discription__wrapper">
-    <div class="description">
+
+// async function posters() {
+//   let poster = await fetch(`http://www.omdbapi.com/?apikey=19c5e51c&s=fire`)
+//   let posterData = await poster.json()
+//   featuredPosters.innerHTML = posterData.map((movie) => {
+//     return `<div class="element__wrapper">
+//   <div class="img__wrapper">
+//     <img src="${movie.Poster}" alt="" />
+//   </div>
+//   <div class="discription__wrapper">
+//     <div class="description">
       
-    </div>
-  </div>
-</div>`
-}
+//     </div>
+//   </div> 
+// </div>`
+//   }).join('')
+// }
+
+// function posterHTML(movie) {
+  
+// }
 
 
